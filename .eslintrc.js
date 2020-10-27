e3bb463440c57060b14b67d434f4fbb5318545a6
cebasypia <cebasypia.tskm8@gmail.com>
2020-10-27 17:11:04 +0900

    Add css-module-types configures

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
     'plugin:import/typescript',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    // 'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:prettier/recommended',
     'prettier',
     'prettier/@typescript-eslint',
