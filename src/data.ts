58411836a53397fc9fd5bfb096fbefee2b60c624
cebasypia <cebasypia.tskm8@gmail.com>
2020-10-27 17:14:23 +0900

    Add sample data

---
+++ src/data.ts
@@ -0,0 +1,41 @@
+const data = [
+  {
+    createdAt: '1分前',
+    id: '1320340917119676416',
+    text:
+      "@mk_ichinoce Maki kuuun おめでとう。。。 ☺\n\nSorry I can't go to Japan that time. \nGood luck for live performance ☺🍀\n\nI hope it make to DVD so I can watch it too \n🥺🍀🍀🍀🍀",
+    user: {
+      id: '72786541',
+      name: '🍀🍀ディラ🍀🍀 🔥12.27『Re-write』🔥',
+      screenName: 'kiriechan',
+      description:
+        'RANDOM FAN ACCOUNT || SixTONES 💎|| 【VOYZ BOY】|| Ra*bits 🐰|| Acid Black Cherry 🍒|| AnCafe || 櫻井圭登 || 大崎 捺希 || あんスタ/テ || 7MEN侍 || 9月🍀🍀',
+      followersCount: 801,
+      friendsCount: 1092,
+      profileImageUrl:
+        'https://pbs.twimg.com/profile_images/1318516254425055232/anq5_8nq_bigger.jpg',
+      uri: 'https://twitter.com/kiriechan',
+    },
+    uri: 'https://twitter.com/kiriechan/status/1320340917119676416',
+  },
+  {
+    createdAt: '1分前',
+    id: '1320340915806822402',
+    text:
+      '私は #パジャマのまま楽しみたい ！おうち時間に飲むコーヒー、みんなはどう楽しみたい？ @GEORGIA_JAPAN をフォロー&amp;ツイートで、新製品 #ジョージアロースタリーブラック とオリジナルマグのセットが当たる！10/26まで！',
+    user: {
+      id: '751782924102406145',
+      name: 'ごすけ😎',
+      screenName: 'itttdblmUwTjLTV',
+      description: '',
+      followersCount: 317,
+      friendsCount: 4994,
+      profileImageUrl:
+        'https://pbs.twimg.com/profile_images/834750396081975297/4jyQQv3t_bigger.jpg',
+      uri: 'https://twitter.com/itttdblmUwTjLTV',
+    },
+    uri: 'https://twitter.com/itttdblmUwTjLTV/status/1320340915806822402',
+  },
+];
+
+export default data;
