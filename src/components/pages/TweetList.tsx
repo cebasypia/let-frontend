b2c5879d079d2ed3aeef1efe9561102d40d2b813
cebasypia <cebasypia.tskm8@gmail.com>
2020-10-31 22:28:05 +0900

    Add SearchForm

--- src/components/pages/TweetList.tsx
+++ src/components/pages/TweetList.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import TweetCard from 'components/organisms/TweetCard';
 import useGetTweets from 'hooks/use-get-tweets';
+import SearchForm from 'components/molecules/SearchForm';
+import TweetCard from 'components/organisms/TweetCard';
 
 const TweetList: React.FC = () => {
   const { tweets } = useGetTweets();
 
   return (
     <div>
+      <SearchForm />
       {tweets.map((tweet) => (
         <TweetCard tweet={tweet} />
       ))}
