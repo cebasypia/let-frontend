d2255831681289efe794139bd917efc940d286ba
cebasypia <cebasypia.tskm8@gmail.com>
2020-10-31 21:16:15 +0900

    Extract FavoriteCounter from TweetBottom

---
+++ src/components/atoms/FavoriteCounter.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
+import color from 'components/scss/color.module.scss';
+import styles from 'components/scss/icon.module.scss';
+
+type Props = {
+  count: number;
+};
+
+const FavoriteCounter: React.FC<Props> = ({ count }) => (
+  <span className={color.subText}>
+    <FontAwesomeIcon className={styles.mr} icon={faHeart} />
+    {count}
+  </span>
+);
+
+export default FavoriteCounter;
--- src/components/molecules/TweetBottom.tsx
+++ src/components/molecules/TweetBottom.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons';
-import { faHeart } from '@fortawesome/free-regular-svg-icons';
 import { faRetweet } from '@fortawesome/free-solid-svg-icons';
 import { Tweet } from 'domains/twitter';
+import FavoriteCounter from 'components/atoms/FavoriteCounter';
 import color from '../scss/color.module.scss';
 import styles from './TweetBottom.module.scss';
 
@@ -14,11 +14,8 @@ const TweetBottom: React.FC<Props> = ({ tweet }) => (
   <div className={styles.wrapper}>
     <a href={tweet.uri}>
       <FontAwesomeIcon className={color.sub} icon={faTwitter} />
-    </a>{' '}
-    <span className={color.subText}>
-      <FontAwesomeIcon className={styles.icon} icon={faHeart} />
-      {tweet.favoriteCount}
-    </span>
+    </a>
+    <FavoriteCounter count={tweet.favoriteCount} />
     <span className={color.subText}>
       <FontAwesomeIcon className={styles.icon} icon={faRetweet} />
       {tweet.retweetCount}
