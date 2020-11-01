18d107617079aa2cfb39dc57c8efc94590c60859
cebasypia <cebasypia.tskm8@gmail.com>
2020-11-01 11:47:28 +0900

    Remove error message

--- src/components/molecules/SearchForm.tsx
+++ src/components/molecules/SearchForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useForm } from 'react-hook-form';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
@@ -10,25 +10,23 @@ type FormData = {
 };
 
 const SearchForm: React.FC = () => {
-  const [test, setTest] = useState('');
-  const { register, handleSubmit, errors } = useForm<FormData>();
+  const { register, handleSubmit } = useForm<FormData>();
   const onSubmit = handleSubmit(({ searchWord }) => {
     /* eslint-disable-next-line */
     console.log(searchWord);
-    setTest(searchWord);
   });
 
   return (
-    <form className={styles.form} onSubmit={onSubmit}>
-      <FontAwesomeIcon className={icon.mr} icon={faSearch} />
-      <input
-        className={styles.input}
-        name="searchWord"
-        type="search"
-        ref={register({ required: true })}
-      />
-      {errors.searchWord && <div>This field is required</div>}
-      <div>{test}</div>
+    <form onSubmit={onSubmit}>
+      <div className={styles.wrapper}>
+        <FontAwesomeIcon className={icon.mr} icon={faSearch} />
+        <input
+          className={styles.input}
+          name="searchWord"
+          type="search"
+          ref={register({ required: true })}
+        />
+      </div>
     </form>
   );
 };
