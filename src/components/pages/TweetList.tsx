5e94dc896ec0829cab5a1a695ee49d841cf32f56
cebasypia <cebasypia.tskm8@gmail.com>
2020-11-03 15:37:28 +0900

    Add SearchFrom wrapper class

--- src/components/pages/TweetList.tsx
+++ src/components/pages/TweetList.tsx
@@ -3,16 +3,19 @@ import { useLocation } from 'react-router-dom';
 import useGetTweets from 'hooks/use-get-tweets';
 import SearchForm from 'components/molecules/SearchForm';
 import TweetCard from 'components/organisms/TweetCard';
+import styles from './TweetList.module.scss';
 
 const TweetList: React.FC = () => {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
-  const word = searchParams.get('word') ?? '';
-  const { tweets, isLoading } = useGetTweets(word);
+  const keyword = searchParams.get('keyword') ?? '';
+  const { tweets, isLoading } = useGetTweets(keyword);
 
   return (
     <div>
-      <SearchForm defaultWord={word} isLoading={isLoading} />
+      <div className={styles.searchFormWrapper}>
+        <SearchForm defaultWord={keyword} isLoading={isLoading} />
+      </div>
       {!isLoading && !tweets.length && <div>Tweet not found</div>}
       {tweets.map((tweet) => (
         <TweetCard key={tweet.id} tweet={tweet} />
