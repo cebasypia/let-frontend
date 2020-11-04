9fad4cdb88c0625fb9b119c830c3ad5533b8e012
cebasypia <cebasypia.tskm8@gmail.com>
2020-11-04 19:02:09 +0900

    Use onClick instead of <a /> to link to twitter

--- src/components/atoms/TweetLink.tsx
+++ src/components/atoms/TweetLink.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { MouseEvent } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 import color from 'components/scss/color.module.scss';
+import icon from 'components/scss/icon.module.scss';
 
 type Props = {
   uri: string;
 };
 
-const TweetLink: React.FC<Props> = ({ uri }) => (
-  <a href={uri}>
-    <FontAwesomeIcon className={color.sub} icon={faTwitter} />
-  </a>
-);
+const TweetLink: React.FC<Props> = ({ uri }) => {
+  const handleClick = (e: MouseEvent<SVGSVGElement>): void => {
+    e.preventDefault();
+    window.open(uri, '_blank');
+  };
+
+  return (
+    <FontAwesomeIcon
+      onClick={handleClick}
+      className={`${color.sub} ${icon.button}`}
+      icon={faTwitter}
+    />
+  );
+};
 
 export default TweetLink;
