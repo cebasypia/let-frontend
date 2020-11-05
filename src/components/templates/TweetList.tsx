4baf062a7218bc3c22f4ca5ac77445dca8d3fa51
cebasypia <cebasypia.tskm8@gmail.com>
2020-11-05 15:24:28 +0900

    Add tweets default view #17

--- src/components/templates/TweetList.tsx
+++ src/components/templates/TweetList.tsx
@@ -3,6 +3,7 @@ import { Link, useLocation } from 'react-router-dom';
 import useGetTweets from 'hooks/use-get-tweets';
 import SearchBox from 'components/organisms/SearchBox';
 import TweetCard from 'components/organisms/TweetCard';
+import EarthImage from 'images/earth.png';
 import styles from './TweetList.module.scss';
 
 const TweetList: React.FC = () => {
@@ -12,15 +13,35 @@ const TweetList: React.FC = () => {
   const { tweets, isLoading } = useGetTweets(keyword);
 
   return (
-    <div>
+    <>
       <SearchBox defaultWord={keyword} isLoading={isLoading} />
-      {!isLoading && !tweets.length && <div>Tweet not found</div>}
-      {tweets.map((tweet) => (
-        <Link className={styles.linkStyle} to={location.pathname + tweet.id}>
-          <TweetCard key={tweet.id} tweet={tweet} />
-        </Link>
-      ))}
-    </div>
+
+      {keyword ? (
+        //  検索ワードが入力されている時
+        <>
+          {!isLoading && !tweets.length && <div>Tweet not found</div>}
+          {tweets.map((tweet) => (
+            <Link
+              className={styles.linkStyle}
+              to={location.pathname + tweet.id}
+            >
+              <TweetCard key={tweet.id} tweet={tweet} />
+            </Link>
+          ))}
+        </>
+      ) : (
+        // 検索ワードが入力されていない時
+        <div className={styles.wrapper}>
+          <div className={styles.hint}>
+            好きな単語を入力して
+            <br />
+            世界のTweetを覗いてみよう！
+          </div>
+
+          <img className={styles.earth} src={EarthImage} alt="earth" />
+        </div>
+      )}
+    </>
   );
 };
 
