bbdf211c3b910db9f75783685adbafde684069de
cebasypia <cebasypia.tskm8@gmail.com>
2020-11-06 22:21:01 +0900

    Create content

--- src/components/pages/TwitterUser.tsx
+++ src/components/pages/TwitterUser.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useGetTwitterUser } from 'hooks/useGetTwitterUser';
+import { useGetTimeline } from 'hooks/useGetTimeline';
+import UserCard from 'components/organisms/UserCard';
+import TweetList from 'components/templates/TweetList';
+import Tag from 'components/atoms/Tag';
 
 type Params = {
   screenName: string;
@@ -9,8 +13,21 @@ type Params = {
 const TwitterUser: React.FC = () => {
   const { screenName } = useParams<Params>();
   const { user, isLoading } = useGetTwitterUser(screenName);
+  const { tweets } = useGetTimeline(screenName);
 
-  return <>{isLoading ? <div>now Loading</div> : <div>{user?.name}</div>}</>;
+  return (
+    <>
+      {user ? (
+        <>
+          <UserCard user={user} />
+          <Tag title="Tweets" />
+          <TweetList tweets={tweets} />
+        </>
+      ) : (
+        !isLoading && <div>User not found</div>
+      )}
+    </>
+  );
 };
 
 export default TwitterUser;
