f3938deda99c661e132ebe2510e156c8fbd63019
cebasypia <cebasypia.tskm8@gmail.com>
2020-11-08 20:38:46 +0900

    Add List

---
+++ src/components/organisms/List.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { ListItem } from 'utils/listItem';
+import { combineStrings } from 'utils/combineStrings';
+import icon from 'components/scss/icon.module.scss';
+import styles from './List.module.scss';
+
+type Props = {
+  className?: string;
+  items: ListItem[];
+};
+
+const List: React.FC<Props> = ({ className = '', items }) => {
+  return (
+    <div className={combineStrings(styles.wrapper, className)}>
+      {items.map((item) => (
+        <Link key={item.to} to={item.to} className={styles.link}>
+          <FontAwesomeIcon className={icon.mr} icon={item.icon} /> {item.title}
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default List;
