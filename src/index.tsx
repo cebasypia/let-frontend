82d15fd3de9fbea2113d9c3c55b6850f49d7c96a
cebasypia <cebasypia.tskm8@gmail.com>
2020-11-09 20:59:09 +0900

    Add Auth0 Provider With History

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import { Auth0Provider } from '@auth0/auth0-react';
+import { Auth0ProviderWithHistory } from 'Auth0ProviderWithHistory';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Auth0Provider
-        domain="auth0-sample-cebasypia.us.auth0.com"
-        clientId="DohnJf5OEbkzhMEuHLkzVYzkZOaRpFQq"
-        redirectUri={window.location.origin}
-      >
+      <Auth0ProviderWithHistory>
         <App />
-      </Auth0Provider>
+      </Auth0ProviderWithHistory>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
