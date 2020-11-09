303ead10fefad328d0633e976ccb36a3628e3482
cebasypia <cebasypia.tskm8@gmail.com>
2020-11-09 15:23:08 +0900

    Add Auth0

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Auth0Provider
+        domain="auth0-sample-cebasypia.us.auth0.com"
+        clientId="DohnJf5OEbkzhMEuHLkzVYzkZOaRpFQq"
+        redirectUri={window.location.origin}
+      >
+        <App />
+      </Auth0Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
