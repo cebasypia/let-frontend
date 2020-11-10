b98c603b8780b08fe6477be65c8ba937a8f2dcd5
cebasypia <cebasypia.tskm8@gmail.com>
2020-11-10 22:42:55 +0900

    Add useGetUser

---
+++ src/domains/let/services/getUser.ts
@@ -0,0 +1,16 @@
+import { User, isUser } from 'domains/let';
+
+export const getUser = async (id: string): Promise<User> => {
+  const url = new URL(`http://localhost:3000/api/v1/users/${id}`);
+  try {
+    const response = await fetch(url.toString());
+    const user = (await response.json()) as User;
+    if (!isUser(user)) {
+      throw Error('API取得エラー: データ型不一致');
+    }
+
+    return user;
+  } catch (error) {
+    throw Error(error);
+  }
+};
---
+++ src/hooks/useGetUser.ts
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+
+import { User, getUser } from 'domains/let';
+
+type ReturnValue = {
+  user?: User;
+  isLoading: boolean;
+};
+
+export const useGetUser = (id: string): ReturnValue => {
+  const [user, setUser] = useState<User>();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const load = async (): Promise<void> => {
+      setIsLoading(true);
+
+      try {
+        const userData = await getUser(id);
+        setUser(userData);
+      } catch (err) {
+        throw new Error(err);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    void load();
+  }, [id]);
+
+  return { user, isLoading };
+};
