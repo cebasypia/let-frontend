c3f705bda45c30cfec4757afb8db3724a7939e7e
cebasypia <cebasypia.tskm8@gmail.com>
2020-11-12 21:56:05 +0900

    Use getUser() instead of auth0's user

--- src/components/pages/UserEdit.tsx
+++ src/components/pages/UserEdit.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
+import { useHistory } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
-import { patchUser } from 'domains/let';
+import { patchUser, getUser } from 'domains/let';
 import ErrorMessage from 'components/atoms/ErrorMessage';
 // import styles from './UserFrom.module.scss';
 
@@ -11,7 +12,8 @@ type FormData = {
 
 const UserFrom: React.FC = () => {
   const { register, handleSubmit, errors, setValue } = useForm<FormData>();
-  const { getAccessTokenSilently } = useAuth0();
+  const { user, getAccessTokenSilently } = useAuth0();
+  const history = useHistory();
 
   const onSubmit = handleSubmit(async ({ name }) => {
     try {
@@ -19,17 +21,19 @@ const UserFrom: React.FC = () => {
         audience: process.env.REACT_APP_AUTH0_AUDIENCE,
       });
       const _ = await patchUser({ user: { name } }, accessToken);
-      /* eslint-disable-next-line */
-      console.log(_);
+      history.push(`/users/${user.sub}`);
     } catch (err) {
       throw new Error(err);
     }
   });
 
-  const { user } = useAuth0();
-
   useEffect(() => {
-    setValue('name', user.name);
+    const setForm = async () => {
+      if (!user) return;
+      const userData = await getUser(user.sub);
+      setValue('name', userData.name);
+    };
+    setForm();
   }, [setValue, user]);
 
   return (
