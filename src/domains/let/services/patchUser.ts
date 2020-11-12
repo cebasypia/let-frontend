06cb4b5749fa789028b09c8ff068c7d5e5716105
cebasypia <cebasypia.tskm8@gmail.com>
2020-11-12 16:31:22 +0900

    Add patchUser

---
+++ src/domains/let/services/patchUser.ts
@@ -0,0 +1,35 @@
+import { User, isUser } from 'domains/let';
+import ky from 'ky';
+
+type UserData = {
+  user: { name: string };
+};
+
+export const patchUser = async (
+  user: UserData,
+  accessToken: string,
+): Promise<User> => {
+  const url = new URL(`${process.env.REACT_APP_BACKEND_DOMAIN}/users`);
+
+  try {
+    const response = await ky.patch(url, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      json: user,
+    });
+    const updatedUser = (await response.json()) as User;
+    if (!isUser(updatedUser)) {
+      throw Error(`API取得エラー: データ型不一致`);
+    }
+    /* eslint-disable-next-line */
+    console.log(updatedUser)
+
+    return updatedUser;
+  } catch (error) {
+    throw Error(error);
+  } finally {
+    /* eslint-disable-next-line */
+    console.log('end')
+  }
+};
