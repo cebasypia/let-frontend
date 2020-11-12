582bfb04b4ca6ce4935fbbed6a0cac11c2ac8c64
cebasypia <cebasypia.tskm8@gmail.com>
2020-11-12 10:02:15 +0900

    Send RegisterUser API  after login

--- src/Auth0ProviderWithHistory.tsx
+++ src/Auth0ProviderWithHistory.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
-import { Auth0Provider } from '@auth0/auth0-react';
+import { Auth0Provider, useAuth0 } from '@auth0/auth0-react';
 import { AppState } from '@auth0/auth0-react/dist/auth0-provider';
 
 export const Auth0ProviderWithHistory: React.FC = ({ children }) => {
@@ -22,8 +22,31 @@ export const Auth0ProviderWithHistory: React.FC = ({ children }) => {
       clientId={clientId}
       redirectUri={window.location.origin}
       onRedirectCallback={onRedirectCallback}
+      audience={process.env.REACT_APP_AUTH0_AUDIENCE}
     >
-      {children}
+      <UserMonitor>{children}</UserMonitor>
     </Auth0Provider>
   );
+};
+
+const UserMonitor: React.FC = ({ children }) => {
+  const { getAccessTokenSilently, user } = useAuth0();
+
+  useEffect(() => {
+    const RegisterUser = async () => {
+      if (!user) return;
+      const accessToken = await getAccessTokenSilently({
+        audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+      });
+
+      const url = new URL(`http://localhost:3000/api/v1/users/${user.sub}`);
+
+      await fetch(url.toString(), {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      });
+    };
+    void RegisterUser();
+  }, [user, getAccessTokenSilently]);
+
+  return <>{children}</>;
 };
