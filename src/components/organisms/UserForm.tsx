4f6aee8c0f17b6b0fdc019744fc8b1eeccb3b853
cebasypia <cebasypia.tskm8@gmail.com>
2020-11-15 22:00:56 +0900

    Add Avatar section

--- src/components/organisms/UserForm.tsx
+++ src/components/organisms/UserForm.tsx
@@ -1,54 +1,72 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useHistory } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
-import { patchUser, getUser } from 'domains/let';
+import { patchUser, User } from 'domains/let';
+import Avatar from 'components/atoms/Avatar';
 import TextForm from 'components/molecules/TextForm';
 import styles from './UserForm.module.scss';
 
 type FormData = {
   name: string;
   description: string;
+  profileImage: string;
 };
 
-const UserForm: React.FC = () => {
-  const { register, handleSubmit, errors, setValue, watch } = useForm<
-    FormData
-  >();
+type Props = {
+  user: User;
+};
+
+const UserForm: React.FC<Props> = ({ user }) => {
+  const { register, handleSubmit, errors, watch } = useForm<FormData>();
   const watchName = watch('name');
   const watchDescription = watch('description');
-  const { user, getAccessTokenSilently } = useAuth0();
+  const [imageUrl, setImageUrl] = useState(user.profileImageUrl);
+
+  const { getAccessTokenSilently } = useAuth0();
   const history = useHistory();
 
-  const onSubmit = handleSubmit(async ({ name }) => {
+  const onSubmit = handleSubmit(async ({ name, description }) => {
     try {
       const accessToken = await getAccessTokenSilently({
         audience: process.env.REACT_APP_AUTH0_AUDIENCE,
       });
-      const _ = await patchUser({ user: { name } }, accessToken);
-      history.push(`/users/${user.sub}`);
+      const _ = await patchUser({ user: { name, description } }, accessToken);
+      history.push(`/users/${user.id}`);
     } catch (err) {
       throw new Error(err);
     }
   });
 
-  useEffect(() => {
-    const setForm = async () => {
-      if (!user) return;
-      const userData = await getUser(user.sub);
-      setValue('name', userData.name);
-    };
-    setForm();
-  }, [setValue, user]);
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = e.target;
+    if (!files) return;
+    setImageUrl(URL.createObjectURL(files[0]));
+  };
 
   return (
     <div className={styles.wrapper}>
       <form onSubmit={onSubmit}>
+        <div className={styles.fileRow}>
+          <label className={styles.fileBox} htmlFor="profileImage">
+            <Avatar src={imageUrl} color="main" size="large" />
+            <input
+              className={styles.file}
+              type="file"
+              name="profileImage"
+              id="profileImage"
+              accept="image/*"
+              onChange={handleOnChange}
+              ref={register()}
+            />
+          </label>
+        </div>
         <TextForm name="name" value={watchName} hint={errors.name?.message}>
           <input
             className={styles.input}
             name="name"
             type="text"
+            defaultValue={user.name}
             ref={register({ required: '名前を入力してください' })}
           />
         </TextForm>
@@ -61,9 +79,13 @@ const UserForm: React.FC = () => {
             className={styles.input}
             name="description"
             type="text"
+            defaultValue={user.description}
             ref={register()}
           />
         </TextForm>
+        <button type="submit" className={styles.submit}>
+          更新
+        </button>
       </form>
     </div>
   );
