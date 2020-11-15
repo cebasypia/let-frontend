a23760ca6fd1757c9f9cd6a13d5adc59b7f7c854
cebasypia <cebasypia.tskm8@gmail.com>
2020-11-15 22:03:07 +0900

    Pass user to UserForm

--- src/components/pages/UserEdit.tsx
+++ src/components/pages/UserEdit.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { useGetUser } from 'hooks/useGetUser';
 import UserForm from 'components/organisms/UserForm';
 
-const UserEdit: React.FC = () => <UserForm />;
+const UserEdit: React.FC = () => {
+  const { user } = useAuth0();
+  const loginUser = useGetUser(user.sub).user;
+
+  return <>{loginUser && <UserForm user={loginUser} />}</>;
+};
 
 export default UserEdit;
